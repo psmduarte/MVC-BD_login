8fe01b5106dccbf5b38acf34ff574ac290af7a6b
Paulo Duarte <psmduarte@live.com>
2018-09-20 00:11:49 +0100

    Criacao do UserService

--- src/main/java/com/duarte/login/model/User.java
+++ src/main/java/com/duarte/login/model/User.java
@@ -1,5 +1,6 @@
 package com.duarte.login.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.management.relation.Role;
@@ -47,22 +48,8 @@ public class User {
 	private int active;
 
 	@ManyToMany(cascade=CascadeType.ALL)
-	private Set<Role> role;
+	private Set<Role> roles;
 
-	public User(int id,
-			@Email(message = "Please provide a valid email") @NotEmpty(message = "Please provide an email") String email,
-			@Length(min = 5, message = "Your password must have ate leats 5 characters") @NotEmpty(message = "Please provide a password") String password,
-			@NotEmpty(message = "Please provide your name") String nome,
-			@NotEmpty(message = "Please provide your last name") String lastName, int active, Set<Role> role) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.password = password;
-		this.nome = nome;
-		this.lastName = lastName;
-		this.active = active;
-		this.role = role;
-	}
 
 	public int getId() {
 		return id;
@@ -112,12 +99,19 @@ public class User {
 		this.active = active;
 	}
 
-	public Set<Role> getRole() {
-		return role;
+	public Set<Role> SetRoles() {
+		return roles;
 	}
 
-	public void setRole(Set<Role> role) {
-		this.role = role;
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
+
+	public void setRoles(HashSet<com.duarte.login.model.Role> hashSet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 	
 }
